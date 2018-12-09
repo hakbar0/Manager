c916f9ac32d410886b2719098d31972e9411f55b
hakbar0 <haseeb2000@hotmail.com>
2018-12-09 19:43:24 +0000

    linking up actions and reducers

--- manager/App.js
+++ manager/App.js
@@ -28,13 +28,4 @@ class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 export default App;
