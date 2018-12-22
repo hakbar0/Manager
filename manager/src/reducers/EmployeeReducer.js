11bf3ed1148e5cfbef80a05b391b82cce0c7c9fe
hakbar0 <haseeb2000@hotmail.com>
2018-12-22 22:49:07 +0000

    firebase as data store

---
+++ manager/src/reducers/EmployeeReducer.js
@@ -0,0 +1,14 @@
+import {
+    EMPLOYEES_FETCH_SUCCESS
+} from '../actions/types';
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, actions) => {
+    switch (actions.type) {
+        case EMPLOYEES_FETCH_SUCCESS:
+            return actions.payload;
+        default:
+            return state;
+    }
+};
